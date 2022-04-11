7171b714721a81b8a002354214760c729f781a25
Isaac Solano <isolanol@ucenfotec.ac.cr>
2022-04-10 19:27:43 -0600

    fixes

--- src/main/java/com/project/springbackend/Controller/ApiController.java
+++ src/main/java/com/project/springbackend/Controller/ApiController.java
@@ -2,32 +2,35 @@ package com.project.springbackend.Controller;
 
 import com.project.springbackend.Domain.User;
 import com.project.springbackend.Model.Model;
-import com.project.springbackend.Model.Response;
 import com.project.springbackend.Service.UserService;
-import org.hibernate.NonUniqueObjectException;
+import com.project.springbackend.Utils.Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.security.crypto.encrypt.Encryptors;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 public class ApiController {
     @Autowired
-    UserService userService;
+    private UserService userService;
     private static final String entityApi = "users";
+    private Utils utils = new Utils();
 
     @PostMapping(value = "/api/" + entityApi, produces = MediaType.APPLICATION_JSON_VALUE)
+    @CrossOrigin(origins = "http://localhost:4200")
     public @ResponseBody Model post(@RequestBody User user) {
+        user.setPassword(this.utils.encodePassword(user.getPassword()));
         userService.save(user);
+        this.utils.cleanUser(user);
         return user;
     }
 
     @GetMapping (value = "/api/" + entityApi + "/{username}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @CrossOrigin(origins = "http://localhost:4200")
     public @ResponseBody Model get(@PathVariable String username){
         User user = userService.retrieve(username);
+        this.utils.cleanUser(user);
         return user;
     }
 
